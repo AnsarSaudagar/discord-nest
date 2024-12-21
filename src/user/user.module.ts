95fb2e5cb1c7494273d87aa859855e923d1207d1
ansar <ansarsaudagar40@gmail.com>
2024-12-21 14:28:11 +0530

    auth controller and service creatd

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,12 +3,14 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './entities/user.entity';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
-  controllers: [UserController],
-  providers: [UserService],
+  controllers: [UserController, AuthController],
+  providers: [UserService, AuthService],
 })
 export class UserModule {}
