5c0cc6420574a4d4fd4bd7ca99cbd682c7ac2082
ansar <ansarsaudagar40@gmail.com>
2024-12-21 15:05:52 +0530

    register created successfully

--- src/user/auth/auth.controller.ts
+++ src/user/auth/auth.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpException,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  async register(@Body() userData: CreateUserDto) {
+    try {
+      const user = await this.authService.register(userData);
+      return { message: 'User created successfully', data: user };
+    } catch (error) {
+      if (error.code === 11000) {
+        const field = error.keyValue.email ? 'Email' : 'Username';
+        throw new HttpException(
+          { message: `${field} Already Exists`, error: 'Bad Request' },
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+
+      throw new HttpException(
+        { message: error.message, error: 'Internal Server Error' },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+}
--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { User } from '../entities/user.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import { CreateUserDto } from '../dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async register(userData: CreateUserDto) {
+
+      const saltRounds = 10;
+      const hashPassword = await bcrypt.hash(userData.password, saltRounds);
+
+      userData.password = hashPassword;
+
+      const newUser = new this.userModel(userData);
+
+      return newUser.save();
+  }
+}
